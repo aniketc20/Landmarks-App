5ba7b26413dcfdd21753ce0c33d7d3bf09466fad
Aniket Choudhary <56171163+aniketsonu@users.noreply.github.com>
2024-10-25 15:41:29 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ANIKET CHOUDHARY on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,16 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by ANIKET CHOUDHARY on 25/10/24.
+//
+
+import Testing
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
